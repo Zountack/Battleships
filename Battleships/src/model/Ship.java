a29a15845391a7b8da42de1267ed42bc436593c5
mcdowall <mcdowall@WS89.ad.u-form.de>
2022-02-02 15:00:05 +0100

    First Commit

---
+++ Battleships/src/model/Ship.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Ship {
+	private boolean isVertical;
+
+	public boolean isVertical() {
+		return isVertical;
+	}
+
+	public void setVertical(boolean isVertical) {
+		this.isVertical = isVertical;
+	}
+
+}
